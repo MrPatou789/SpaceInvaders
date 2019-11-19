6b9d6b26959b9b27d3ac8da92cfdc3dd78a49c23
Anthony <anthony87131@hotmail.fr>
2019-11-19 17:27:01 +0100

    WIP : add aliens

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,16 @@
 class UFO {
-    constructor(){
-        this._x = null,
-        this._y = null,
-        this._height = null,
-        this._width = null,
-        this._status = true
+    constructor() {
+        this._id = null,
+            this._x = null,
+            this._y = null,
+            this._height = null,
+            this._width = null,
+            this._status = true
     }
 
+    get id() {
+        return this._id
+    }
     get x() {
         return this._x
     }
@@ -42,38 +46,84 @@ class UFO {
 }
 
 class Alien extends UFO {
-    constructor(){
+    constructor(id) {
         super()
+        this._id = id
     }
 }
 
 class Fighter extends UFO {
-    constructor(){
+    constructor() {
         super()
     }
 }
 
 class Missile extends UFO {
-    constructor(){
+    constructor() {
         super()
     }
 }
 
 
 var M = {
-    fighter = null,
-    alliens = [],
-    missiles  = []
+    fighter: null,
+    aliens: [],
+    missiles: []
 }
 
 var V = {
-    init : function (){
-    }
+    direction : 'right',
+
+    displayAlien: function (id) {
+        const aliens = document.getElementById('aliens')
+        const template = document.getElementById('alien')
+        const clone = document.importNode(template.content, true);
+        const div = clone.querySelector("div")
+        div.setAttribute('data', id)
+
+        aliens.appendChild(clone);
+    },
+
+
+    step: function () {
+        requestAnimationFrame(function () {
+            const aliens = document.getElementById('aliens')
+            if (V.direction === 'right' && parseInt(aliens.style.marginRight) > 0) {
+                aliens.style.marginLeft = `${parseInt(aliens.style.marginLeft) + 1}px`
+                aliens.style.marginRight = `${parseInt(aliens.style.marginRight) - 1}px`
+            }else{
+                if( V.direction === 'right'){
+                    aliens.style.marginTop = `${parseInt(aliens.style.marginTop) + 20}px`
+                }
+                V.direction = 'left'
+            }
+
+            if (V.direction === 'left' && parseInt(aliens.style.marginLeft) > 0) {
+                aliens.style.marginLeft = `${parseInt(aliens.style.marginLeft) - 1}px`
+                aliens.style.marginRight = `${parseInt(aliens.style.marginRight) + 1}px`
+            }else{
+                if( V.direction === 'left'){
+                    aliens.style.marginTop = `${parseInt(aliens.style.marginTop) + 20}px`
+                }
+                V.direction = 'right'
+            }
+            
+            V.step()
+        });
+    },
 }
 
 var C = {
-    init : function (){
-        V.init()
+    init: function () {
+        for (let i = 0; i < 57; i++) {
+            M.aliens.push(new Alien(i))
+            V.displayAlien(i)
+
+        }
+
+        requestAnimationFrame(function () {
+            V.step()
+        });
     }
 }
 
